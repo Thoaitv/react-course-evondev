7c7eb478ec10dd841b152ae3a6b150bb886d2288
Thoai <thoaitavan@gmail.com>
2022-09-09 22:29:40 +0700

    loading and debound basic css

--- src/components/news/HackerNews.js
+++ src/components/news/HackerNews.js
@@ -1,17 +1,37 @@
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
+import lodash from 'lodash';
 
 const HackerNews = () => {
   const [hints, setHints] = useState([]);
-  const [query, setQuery] = useState('react');
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
   const handleFetchData = useRef({});
 
+  const handleUpdateQuery = lodash.debounce((e) => {
+    setQuery(e.target.value);
+  }, 500);
+
   handleFetchData.current = async () => {
-    const respons = await axios.get(
-      `https://hn.algolia.com/api/v1/search?query=${query}`
-    );
-    console.log('handleFetchData: ', respons);
-    setHints(respons.data?.hits || []);
+    setLoading(true);
+
+    try {
+      const respons = await axios.get(
+        `https://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      // `http://103.216.113.130:8008/api/v1/category/get_all_parent/`;
+      console.log('handleFetchData: ', respons);
+
+      setHints(respons.data?.hits || []);
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setErrorMessage(`The error happend ${error}`);
+    }
+
     //respons.data?.hits
     //<=>
     // if (respons.data) {
@@ -25,16 +45,31 @@ const HackerNews = () => {
   }, [query]);
 
   return (
-    <div>
+    <div className="bg-white mx-auto mt-5 p-5 rounded-lg shadow-md w-3/4">
       <input
         type="text"
-        className="border border-green-500 text-black p-5 mb-5"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        className="border border-gray-500 p-5 mb-5 block rounded-md w-full focus:border-blue-400 transition-all"
+        defaultValue={query}
+        onChange={handleUpdateQuery}
+        placeholder="typing"
       />
 
-      {hints.length > 0 &&
-        hints.map((item, index) => <h3 key={item.title}>{item.title}</h3>)}
+      {loading && (
+        <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-4 border-r-transparent animate-spin mx-auto my-10"></div>
+      )}
+
+      {!loading && errorMessage && <p>{errorMessage}</p>}
+
+      <div className="flex flex-wrap gap-5">
+        {!loading &&
+          hints.length > 0 &&
+          hints.map((item, index) => (
+            <h3 key={item.title} className="p-3 bg-gray-100 rounded-md ">
+              {item.title}
+            </h3>
+          ))}
+        {/* hints.map((item, index) => <h3>{item.parent}</h3>)} */}
+      </div>
     </div>
   );
 };
